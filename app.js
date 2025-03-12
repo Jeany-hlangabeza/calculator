38eaf6a6ddca0d55beb497fbcbca98781ea293fb
Jeany-hlangabeza <jeanyh144@gmail.com>
2025-03-12 20:20:21 +0000

    added some functionality to the calculator
    made the calculator work and able to make calculations

--- app.js
+++ app.js
@@ -16,8 +16,49 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   document.addEventListener("keydown", function (event) {
+    const allowedKeys = "1234567890./*+-%()";
+
     if (!inputField.matches(":focus")) {
       keepFocus();
     }
+
+    if (["Backspace", "Enter", "Delete"].includes(event.key)) {
+      return;
+    }
+
+    if (!allowedKeys.includes(event.key)) {
+      event.preventDefault();
+    }
   });
+
+  document.querySelector(".equals").addEventListener("click", function () {
+    calculateResult();
+  });
+
+  inputField.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      calculateResult();
+    }
+  });
+
+  document.querySelector(".AC").addEventListener("click", function () {
+    inputField.removeAttribute("disabled");
+    inputField.value = "";
+    inputField.setAttribute("disabled", "true");
+  });
+
+  document.querySelector(".DE").addEventListener("click", function () {
+    inputField.removeAttribute("disabled");
+    inputField.value = inputField.value.slice(0, -1);
+    inputField.setAttribute("disabled", "true");
+  });
+
+  function calculateResult() {
+    try {
+      inputField.value = eval(inputField.value);
+    } catch (error) {
+      inputField.value = "Error";
+    }
+  }
 });
