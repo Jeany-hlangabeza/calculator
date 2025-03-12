26db95a905e28bf496bd388e1d11fe98976d4db2
Jeany-hlangabeza <jeanyh144@gmail.com>
2025-03-12 19:00:58 +0000

    added focus to input box

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const inputField = document.querySelector(".user-input");
 
+  function keepFocus() {
+    inputField.focus();
+    inputField.setSelectionRange(
+      inputField.value.length,
+      inputField.value.length
+    );
+  }
+
+  keepFocus();
+
+  inputField.addEventListener("blur", function () {
+    setTimeout(keepFocus, 10);
+  });
+
+  document.addEventListener("keydown", function (event) {
+    if (!inputField.matches(":focus")) {
+      keepFocus();
+    }
+  });
+});
